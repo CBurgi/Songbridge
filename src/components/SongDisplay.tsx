d1080add83866caa501caaaf01dffb285c657f5c
cburgi <cburgi2003@gmail.com>
2026-05-31 21:29:08 -0400

    did some refactoring, added platform images below results

--- src/components/SongDisplay.tsx
+++ src/components/SongDisplay.tsx
@@ -1,58 +1,67 @@
-import { states } from "@/functions/objects";
-import { useState } from "react";
+import { SongData } from "@/functions/objects";
+import { classicNameResolver } from "typescript";
 
-type DisplayProps = {
-  song: string;
-  artist: string;
-  album: string;
-  img_link: string;
-};
-export default function SongDisplay({ song, artist, album, img_link }: DisplayProps) {
+export default function SongDisplay({ song }: { song: SongData }) {
   return (
 
     <div className="
       flex
-      flex-col
-      gap-6
       p-7
     ">
-      <div className="flex flex-col bg-card border border-card-line shadow-2xs rounded-xl">
-        <div className="p-4  ">
-          <div className="
-          flex
-          flex-col
-          items-center
-          gap-6
-          md:flex-row
-          md:gap-8
-          rounded-2xl
-        ">
-            <div>
-              <img
-                className="size-24 shadow-xl rounded-lg"
-                alt={`Album art for ${album}`}
-                src={img_link} /
-              >
-            </div>
-            <div className="grid">
-              <span className="text-xl font-medium">{song}</span>
-              <span className="text-md font-medium text-sky-500">{artist}</span>
-              <span className="
-              flex
-              gap-2
-              font-medium
-              text-gray-600
-              dark:text-gray-400
-            ">{album}</span>
-              {false &&
-                (
-                  <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-                    <span>No. 4</span>
-                    <span>·</span>
-                    <span>2025</span>
+      <div>
+        <div className="flex flex-col bg-card border border-card-line shadow-2xs rounded-xl">
+          <div className="bg-card border border-card-line shadow-2xs rounded-xl">
+            <div className="p-4">
+              <div className="
+                flex
+                flex-col
+                items-center
+                gap-6
+                md:flex-row
+                md:gap-8
+                rounded-2xl
+              ">
+                <img
+                  className="size-24 shadow-xl rounded-lg"
+                  alt={`Album art for ${song.songItem.album}`}
+                  src={song.albumArtURL} /
+                >
+                <div className="grid">
+                  <span className="text-xl font-medium">{song.songItem.name}</span>
+                  <span className="text-md font-medium text-sky-500">{song.songItem.artists.join(', ')}</span>
+                  <span className="
+                    flex
+                    font-medium
+                    text-gray-600
+                    dark:text-gray-400
+                  ">
+                    {song.songItem.album}
                   </span>
-                )
-              }
+                  {false &&
+                    (
+                      <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+                        <span>No. 4</span>
+                        <span>·</span>
+                        <span>2025</span>
+                      </span>
+                    )
+                  }
+                </div>
+              </div>
+              <span className="p-2 flex flex-row gap-4">
+                {
+                  song.extURLs.map((p) => {
+                    const platform = p.platform
+                    return (
+                      <img
+                        className="size-10 shadow-md rounded-sm"
+                        alt={`${platform} logo`}
+                        src={`/images/${platform}.png`}
+                      />
+                    )
+                  })
+                }
+              </span>
             </div>
           </div>
         </div>
--- src/functions/abstract-api.ts
+++ src/functions/abstract-api.ts
@@ -12,7 +12,6 @@ export default abstract class AbstractApi {
   async APIGET(url: URL): Promise<any> {
     const token = await getToken(this.platform); 
 
-    console.log(url.href)
     const res = await fetch(url, {
       method: 'GET',
       headers: { 'Authorization': `Bearer ${token}` }
@@ -20,15 +19,31 @@ export default abstract class AbstractApi {
     if (!res.ok) throw new Error(this.platform + ' API error: ' + res.status);
 
     const json = await res.json();
-    console.log(json);
     return json;
+  }
+
+  GetAlbumArtURL(arts: any[]): string {
+    const artsList = [...arts, {
+      url: '',
+      height: 0,
+    }]
+    const maxQualityArt: any = artsList.reduce((p, c) => {
+      return (p && p.height > c.height) ? p : c
+    })
+
+    return maxQualityArt.url;
   }
 
   abstract ParseSong(song: object): SongData;
 
   abstract ParseUrlForID(url: string): string;
 
-  abstract GetSongByID(id: string): Promise<SongData[]>;
+  abstract GetSongByID(id: string): Promise<SongData>;
 
-  abstract SearchSong(name: string, artists: Array<string>, album: string): Promise<Array<SongData>>;
+  async GetSongByURL(url: string): Promise<SongData> {
+    const id: string = this.ParseUrlForID(url);
+    return await this.GetSongByID(id);
+  }
+
+  abstract SearchSong(name: string, artists: Array<string>, album: string): Promise<SongData[]>;
 }
