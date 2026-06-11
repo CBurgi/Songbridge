d2137153be4971c6bcc903937e064b0d8e6f2490
cburgi <cburgi2003@gmail.com>
2026-06-10 22:49:22 -0400

    made the app always sm

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,22 +28,22 @@ export default function Header({page}: {page: string}) {
 
   return (
     <>
-      <header className="relative flex flex-wrap sm:justify-start sm:flex-nowrap w-full py-3 bg-navbar border-b border-navbar-line">
-        <nav className="max-w-340 w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
-          <div className="flex items-center justify-between">
+      <header className=" relative flex justify-start flex-nowrap w-full py-3 bg-navbar border-b border-navbar-line ">
+        <nav className=" w-full px-4 flex items-center ">
+          <div className="flex w-full items-center justify-between">
             <a href="/" aria-label="Brand">
               <img className="size-10 rounded-md" src="/images/logo.png" alt="Logo" />
             </a>
             <a href="/" aria-label="Brand">
               <p className="text-xl font-bold text-center">SongBridge</p>
             </a>
-            <button type="button" className="hs-collapse-toggle relative size-10 flex justify-center items-center gap-x-2 rounded-lg bg-layer border border-layer-line text-layer-foreground shadow-2xs hover:bg-layer-hover focus:outline-hidden focus:bg-layer-focus disabled:opacity-50 disabled:pointer-events-none" id="hs-navbar-collapse" aria-expanded="false" aria-controls="hs-navbar" aria-label="Toggle navigation" data-hs-collapse="#hs-navbar">
+            <button type="button" className=" hs-collapse-toggle relative size-10 flex justify-center items-center gap-x-2 rounded-lg bg-layer border border-layer-line text-layer-foreground shadow-2xs " id="hs-navbar-collapse" aria-expanded="false" aria-controls="hs-navbar" aria-label="Toggle navigation" data-hs-collapse="#hs-navbar">
               <svg className="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
               <svg className="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
             </button>
           </div>
-          <div id="hs-navbar" className="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block" aria-labelledby="hs-navbar-collapse" role="region">
-            <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+          <div id="hs-navbar" className="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow block" aria-labelledby="hs-navbar-collapse" role="region">
+            <div className=" flex gap-5 flex-row items-center justify-end mt-0 ps-5 ">
               <HsLink link={pages.search} pageName='Search' />
               <HsLink link={pages.about} pageName='About' />
               <HsLink link={pages.supportedPlatforms} pageName='Supported Platforms' />
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
 export default function About() {
   return (
     <>
+      <p className="text-2xl font-bold pt-6 pb-3 text-center">About</p>
     </>
   )
 }
