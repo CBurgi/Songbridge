2377c894d85c3fb6ef6d2c7afdf9a7d7bf631f46
cburgi <cburgi2003@gmail.com>
2026-06-25 16:25:09 -0400

    moved index.html, updated components to be buildable, added build script

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,8 +38,8 @@ export default function Header({page}: {page: string}) {
               <p className="text-xl font-bold text-center">SongBridge</p>
             </a>
             <button type="button" className=" hs-collapse-toggle relative size-10 flex justify-center items-center gap-x-2 rounded-lg bg-layer border border-layer-line text-layer-foreground shadow-2xs " id="hs-navbar-collapse" aria-expanded="false" aria-controls="hs-navbar" aria-label="Toggle navigation" data-hs-collapse="#hs-navbar">
-              <svg className="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
-              <svg className="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
+              <svg className="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
+              <svg className="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
             </button>
           </div>
           <div id="hs-navbar" className=" hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow block " aria-labelledby="hs-navbar-collapse" role="region">
